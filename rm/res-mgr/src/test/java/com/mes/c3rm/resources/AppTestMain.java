12e8929baf89689b6b51cf9d9e6b2e0554fedcb7
mes <jivan.madtha@gmail.com>
2016-02-02 08:28:58 -0800

    updated test uri to localhost:2375

--- rm/res-mgr/src/test/java/com/mes/c3rm/resources/AppTestMain.java
+++ rm/res-mgr/src/test/java/com/mes/c3rm/resources/AppTestMain.java
@@ -7,7 +7,7 @@ public class AppTestMain {
 	public static void main(String[] args) {
 		ContainerModel containerModel = new ContainerModel();
 		containerModel.setImageName("busybox:latest");
-		ResourceManagerImpl resourceManger = new ResourceManagerImpl("http://172.17.42.1:2375");
+		ResourceManagerImpl resourceManger = new ResourceManagerImpl("http://127.0.0.1:2375");
 		String containerId = resourceManger.createContainer(containerModel);
 		System.out.println("The new container Id: "+ containerId);
 		
